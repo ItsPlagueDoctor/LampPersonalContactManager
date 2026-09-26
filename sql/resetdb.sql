c367d7152dec56774185de7035bc0697b933397c
IRllyDntKnow <neriezra59@gmail.com>
2026-09-26 12:41:31 -0400

    Update seeded users to use hashed passwords

--- sql/resetdb.sql
+++ sql/resetdb.sql
@@ -67,26 +67,23 @@ INSERT INTO Roles (RoleName)
 VALUES ('Admin'), ('User');
 
 -- Seed Sample Users
--- passwords need to be hashed in future,
--- contact api dev for this later.
 INSERT INTO `Users` (`FirstName`, `LastName`, `Login`, `Password`, RoleID) VALUES
-('Application','Administrator', 'root', 'COP4331', (SELECT ID FROM Roles WHERE RoleName = 'Admin')),
-('Rick', 'Leinecker', 'RickL', 'COP4331', (SELECT ID FROM Roles WHERE RoleName = 'User')),
-('Sam', 'Hill', 'SamHAdmin', 'Test', (SELECT ID FROM Roles WHERE RoleName = 'Admin')),
-('Ezra', 'Neri', 'EzraNAdmin', 'COP4331', (SELECT ID FROM Roles WHERE RoleName = 'Admin')),
-('John', 'Aedo', 'JohnA', 'COP4331', (SELECT ID FROM Roles WHERE RoleName = 'User')),
-('Ezra', 'Neri', 'EzraN', 'COP4331', (SELECT ID FROM Roles WHERE RoleName = 'User')),
-('Sam', 'Hill', 'SamH', 'COP4331', (SELECT ID FROM Roles WHERE RoleName = 'User'));
+('Corey', 'Pittman', 'CoreyP', '$2y$12$HbF.pz64xftezcW3eE9P8e0aSrm9hsfIv0cD6YXVYGRWStePIceoS', (SELECT ID FROM Roles WHERE RoleName = 'User'));
+('Application','Administrator', 'root', '$2y$12$1pVoZlAAuBo.OELvC4p6pO07FPTAYeBAZLZKIh7UyKffY.9yweABu', (SELECT ID FROM Roles WHERE RoleName = 'Admin')),
+('Graham', 'Davis', 'GrahamD', '$2y$12$6kvKQwOYg2xoNVllnzCK2OYehYax6RfK5qLRophrQZzSTPDL0IBxK', (SELECT ID FROM Roles WHERE RoleName = 'User')),
+('Ezra', 'Neri', 'EzraNAdmin', '$2y$12$o/.GD3Ce59rs2maG9Yn2G.bIRtl7LwcbasrJOCnNK.0JRnSE2ovpu', (SELECT ID FROM Roles WHERE RoleName = 'Admin')),
+('Elier', 'Aguilar', 'ElierA', '$2y$12$Qs9ROPIz8k0z1GRJRn/Exu16vEdw.rWyeK7MgDM.Z7joMjmLfSiC6', (SELECT ID FROM Roles WHERE RoleName = 'User'));
 
--- Seed Sample Contacts for User 6 (EzraN)
+
+-- Seed Sample Contacts for User 3 (GrahamD)
 INSERT INTO `Contacts` (`FirstName`,`LastName`, `UserID`) VALUES
-('Graham','Davis', 6),
-('Joshua','Sar-Shalom', 6),
-('Elier', 'Aguilar', 6),
-('Caleb','Kimondo', 6),
-('John','Aedo', 6);
+('Graham','Davis', 3),
+('Joshua','Sar-Shalom', 3),
+('Elier', 'Aguilar', 3),
+('Caleb','Kimondo', 3),
+('John','Aedo', 3);
 
--- Seed Sample Contacts for User 5 (JohnA)
+-- Seed Sample Contacts for User 5 (ElierA)
 INSERT INTO `Contacts` (`FirstName`,`LastName`, `UserID`) VALUES
 ('Graham','Davis', 5),
 ('Joshua','Sar-Shalom', 5),
